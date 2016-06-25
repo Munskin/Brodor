3de20cd68482c0c6d63107f15fccd2b8c024364b
Sam Huizeling <hello@samhuizeling.nl>
2016-06-25 22:20:41 +0200

    first attempt fb login

--- app/js/firebase.js
+++ app/js/firebase.js
@@ -1,19 +1,59 @@
 $(function() {
-    var myFirebase = firebase.database().ref();
+    var myFirebase = firebase.database().ref('/messages');
     var textInput = document.querySelector('.user-input');
     var sendButton = document.querySelector('.send-button');
+
+    function toggleSignIn() {
+        if (!firebase.auth().currentUser) {
+            // [START createprovider]
+            var provider = new firebase.auth.FacebookAuthProvider();
+            // [END createprovider]
+            provider.addScope('public_profile');
+             // [START signin]
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+            }).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+            });
+            // [END signin]
+        } else {
+            // [START signout]
+            firebase.auth().signOut();
+            // [END signout]
+        }
+    };
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            username = user.displayName;
+            email = user.email;
+            photoURL = user.photoURL;
+        } else {
+            // User is signed out.
+        }
+    });
 
     function sendMessage() {
         var msgText = textInput.value;
         myFirebase.push({
             message: msgText,
-            username: "Sam"
+            username: username
         });
         textInput.value = "";
-    }
+    };
 
     sendButton.addEventListener("click", function() {
-           sendMessage();
+        toggleSignIn();
     });
 
     textInput.addEventListener("keyup", function(event) {

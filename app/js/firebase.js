4e647b9ee1c63a0975b356de64e20e8634f536da
Sam Huizeling <hello@samhuizeling.nl>
2016-06-25 10:47:45 +0200

    push messages working
    using own database

--- app/js/firebase.js
+++ app/js/firebase.js
@@ -1,34 +1,46 @@
 $(function() {
-    // get messages from database
-    firebase.database().ref('/').on('value', function(messages) {
-        
-        // traverse messages function
-        function traverse(obj) {
-            for (var prop in obj) {
-                var message = obj[prop].message;
-                var user = obj[prop].username;
-                
-                // populate function with data
-                createChatElements(user, message);
-            }
-        }
-        // call function
-        traverse(messages.val());
+    var myFirebase = firebase.database().ref();
+    var textInput = document.querySelector('.user-input');
+    var sendButton = document.querySelector('.send-button');
+
+    sendButton.addEventListener("click", function() {
+        var msgText = textInput.value;
+        myFirebase.push({
+            message: msgText,
+            username: "Sam"
+        });
+        textInput.value = "";
     });
 
-    // build html for chat elements
-    function createChatElements(user, message) {
-        var chatHTML = '<div class="chat-message">';
-        chatHTML += '<h5 class="chat-message-username">' + user + '</h5>';
-        chatHTML += '<p class="chat-message-message">' + message + '</p>';
-        chatHTML += '</div>';
+    /** Function to add a data listener **/
+    var startListening = function() {
+        myFirebase.on('child_added', function(snapshot) {
+            var msg = snapshot.val();
 
-        // function to append chat
-        appendChatElements(chatHTML);
+            var message = msg.message;
+            var user = msg.username;
+
+            // populate function with data
+            createChatElements(user, message);
+
+            // build html for chat elements
+            function createChatElements(user, message) {
+                var chatHTML = '<div class="chat-message">';
+                chatHTML += '<h5 class="chat-message-username">' + user + '</h5>';
+                chatHTML += '<p class="chat-message-message">' + message + '</p>';
+                chatHTML += '</div>';
+
+                // function to append chat
+                appendChatElements(chatHTML);
+            }
+
+            // append chatHTML to chat-wrapper
+            function appendChatElements(chat) {
+                $('.chat-window').append(chat);
+            }
+        });
     }
 
-    // append chatHTML to chat-wrapper
-    function appendChatElements(chat) {
-        $('.chat-window').append(chat);
-    }
+    // Begin listening for data
+    startListening();
 });
